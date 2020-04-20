65f05f3feafc41eae061e64a121dd6e71e5c91fc
Zrc123456 <2227068180@qq.com>
2020-04-20 09:27:47 +0800

    提交

--- js/xoyo.js
+++ js/xoyo.js
@@ -162,10 +162,7 @@ $('.word-right').on('click', function () {
             'background-position': '-281px 0px'
         })
     }
-    console.log(index2);
-
     $('.word-video video').eq(index2).fadeIn(500).siblings().fadeOut()
-
     $('.word-daohang li').eq(index2).animate({
         marginTop: 20
     }, 500, function () {
@@ -174,6 +171,482 @@ $('.word-right').on('click', function () {
         }, 20)
     })
 })
+$('.word-daohang li').eq(0).hover(function () {
+    $(this).css({
+        'background-position': '-368px 0px'
+    })
+}, function () {
+    $(this).css({
+        'background-position': '0px 0px'
+    })
+})
+$('.word-daohang li').eq(1).hover(function () {
+    $(this).css({
+        'background-position': '-426px 0px'
+    })
+}, function () {
+    $(this).css({
+        'background-position': '-58px 0px'
+    })
+})
+$('.word-daohang li').eq(2).hover(function () {
+    $(this).css({
+        'background-position': '-481px 0px'
+    })
+}, function () {
+    $(this).css({
+        'background-position': '-114px 0px'
+    })
+})
+$('.word-daohang li').eq(3).hover(function () {
+    $(this).css({
+        'background-position': '-538px 0px'
+    })
+}, function () {
+    $(this).css({
+        'background-position': '-169px 0px'
+    })
+})
+$('.word-daohang li').eq(4).hover(function () {
+    $(this).css({
+        'background-position': '-593px 0px'
+    })
+}, function () {
+    $(this).css({
+        'background-position': '-226px 0px'
+    })
+})
+$('.word-daohang li').eq(5).hover(function () {
+    $(this).css({
+        'background-position': '-648px 0px'
+    })
+}, function () {
+    $(this).css({
+        'background-position': '-281px 0px'
+    })
+})
 let wordMove = setInterval(function () {
     $('.word-right').click()
 }, 5000)
+$('.zh-daohang li').eq(0).hover(function () {
+    $(this).css({
+        'background-position': '0px -40px'
+    })
+}, function () {
+    $(this).css({
+        'background-position': '0px 0px'
+    })
+})
+$('.zh-daohang li').eq(1).hover(function () {
+    $(this).css({
+        'background-position': '-88px -40px'
+    })
+}, function () {
+    $(this).css({
+        'background-position': '-88px 0px'
+    })
+})
+$('.zh-daohang li').eq(2).hover(function () {
+    $(this).css({
+        'background-position': '-176px -40px'
+    })
+}, function () {
+    $(this).css({
+        'background-position': '-176px 0px'
+    })
+})
+$('.zh-daohang li').eq(3).hover(function () {
+    $(this).css({
+        'background-position': '-264px -40px'
+    })
+}, function () {
+    $(this).css({
+        'background-position': '-264px 0px'
+    })
+})
+$('.zh-daohang li').eq(4).hover(function () {
+    $(this).css({
+        'background-position': '-352px -40px'
+    })
+}, function () {
+    $(this).css({
+        'background-position': '-352px 0px'
+    })
+})
+$('.zh-daohang li').eq(5).hover(function () {
+    $(this).css({
+        'background-position': '-440px -40px'
+    })
+}, function () {
+    $(this).css({
+        'background-position': '-440px 0px'
+    })
+})
+$('.zh-daohang li').eq(6).hover(function () {
+    $(this).css({
+        'background-position': '-528px -40px'
+    })
+}, function () {
+    $(this).css({
+        'background-position': '-528px 0px'
+    })
+})
+$('.zh-daohang li').eq(7).hover(function () {
+    $(this).css({
+        'background-position': '-616px -40px'
+    })
+}, function () {
+    $(this).css({
+        'background-position': '-616px 0px'
+    })
+})
+$('.zh-daohang li').eq(8).hover(function () {
+    $(this).css({
+        'background-position': '-704px -40px'
+    })
+}, function () {
+    $(this).css({
+        'background-position': '-704px 0px'
+    })
+})
+$('.zh-daohang li').eq(9).hover(function () {
+    $(this).css({
+        'background-position': '-792px -40px'
+    })
+}, function () {
+    $(this).css({
+        'background-position': '-792px 0px'
+    })
+})
+$('.zh-daohang li').eq(10).hover(function () {
+    $(this).css({
+        'background-position': '-880px -40px'
+    })
+}, function () {
+    $(this).css({
+        'background-position': '-880px 0px'
+    })
+})
+$('.zh-daohang li').eq(11).hover(function () {
+    $(this).css({
+        'background-position': '-968px -40px'
+    })
+}, function () {
+    $(this).css({
+        'background-position': '-968px 0px'
+    })
+})
+$('.zh-daohang li').eq(12).hover(function () {
+    $(this).css({
+        'background-position': '-1056px -40px'
+    })
+}, function () {
+    $(this).css({
+        'background-position': '-1056px 0px'
+    })
+})
+$('.zh-daohang li').eq(13).hover(function () {
+    $(this).css({
+        'background-position': '-1144px -40px'
+    })
+}, function () {
+    $(this).css({
+        'background-position': '-1141px 0px'
+    })
+})
+$('.zh-daohang li').eq(14).hover(function () {
+    $(this).css({
+        'background-position': '-1232px -40px'
+    })
+}, function () {
+    $(this).css({
+        'background-position': '-1232px 0px'
+    })
+})
+$('.zh-daohang li').click(function () {
+    let index = $(this).index()
+    $('.zh-hua img').eq(index).fadeIn(500).siblings().fadeOut(500)
+})
+let index4 = 0
+$('.zh-right').on('click', function () {
+    index4++
+    if (index4 > $('.zh-hua img').length - 1) {
+        index4 = 0
+    }
+    $('.zh-hua img').eq(index4).fadeIn(500).siblings().fadeOut()
+    if (index4 == 0) {
+        $('.zh-daohang li').eq(0).css({
+            'background-position': '0px -40px'
+        })
+    } else {
+        $('.zh-daohang li').eq(0).css({
+            'background-position': '0px 0px'
+        })
+    }
+    if (index4 == 1) {
+        $('.zh-daohang li').eq(1).css({
+            'background-position': '-88px -40px'
+        })
+    } else {
+        $('.zh-daohang li').eq(1).css({
+            'background-position': '-88px 0px'
+        })
+    }
+    if (index4 == 2) {
+        $('.zh-daohang li').eq(2).css({
+            'background-position': '-176px -40px'
+        })
+    } else {
+        $('.zh-daohang li').eq(2).css({
+            'background-position': '-176px 0px'
+        })
+    }
+    if (index4 == 3) {
+        $('.zh-daohang li').eq(3).css({
+            'background-position': '-264px -40px'
+        })
+    } else {
+        $('.zh-daohang li').eq(3).css({
+            'background-position': '-264px 0px'
+        })
+    }
+    if (index4 == 4) {
+        $('.zh-daohang li').eq(4).css({
+            'background-position': '-352px -40px'
+        })
+    } else {
+        $('.zh-daohang li').eq(4).css({
+            'background-position': '-352px 0px'
+        })
+    }
+    if (index4 == 5) {
+        $('.zh-daohang li').eq(5).css({
+            'background-position': '-440px -40px'
+        })
+    } else {
+        $('.zh-daohang li').eq(5).css({
+            'background-position': '-440px 0px'
+        })
+    }
+    if (index4 == 6) {
+        $('.zh-daohang li').eq(6).css({
+            'background-position': '-528px -40px'
+        })
+    } else {
+        $('.zh-daohang li').eq(6).css({
+            'background-position': '-528px 0px'
+        })
+    }
+    if (index4 == 7) {
+        $('.zh-daohang li').eq(7).css({
+            'background-position': '-616px -40px'
+        })
+    } else {
+        $('.zh-daohang li').eq(7).css({
+            'background-position': '-616px 0px'
+        })
+    }
+    if (index4 == 8) {
+        $('.zh-daohang li').eq(8).css({
+            'background-position': '-704px -40px'
+        })
+    } else {
+        $('.zh-daohang li').eq(8).css({
+            'background-position': '-704px 0px'
+        })
+    }
+    if (index4 == 9) {
+        $('.zh-daohang li').eq(9).css({
+            'background-position': '-792px -40px'
+        })
+    } else {
+        $('.zh-daohang li').eq(9).css({
+            'background-position': '-792px 0px'
+        })
+    }
+    if (index4 == 10) {
+        $('.zh-daohang li').eq(10).css({
+            'background-position': '-880px -40px'
+        })
+    } else {
+        $('.zh-daohang li').eq(10).css({
+            'background-position': '-880px 0px'
+        })
+    }
+    if (index4 == 11) {
+        $('.zh-daohang li').eq(11).css({
+            'background-position': '-968px -40px'
+        })
+    } else {
+        $('.zh-daohang li').eq(11).css({
+            'background-position': '-968px 0px'
+        })
+    }
+    if (index4 == 12) {
+        $('.zh-daohang li').eq(12).css({
+            'background-position': '-1056px -40px'
+        })
+    } else {
+        $('.zh-daohang li').eq(12).css({
+            'background-position': '-1056px 0px'
+        })
+    }
+    if (index4 == 13) {
+        $('.zh-daohang li').eq(13).css({
+            'background-position': '-1144px -40px'
+        })
+    } else {
+        $('.zh-daohang li').eq(13).css({
+            'background-position': '-1144px 0px'
+        })
+    }
+    if (index4 == 14) {
+        $('.zh-daohang li').eq(14).css({
+            'background-position': '-1232px -40px'
+        })
+    } else {
+        $('.zh-daohang li').eq(14).css({
+            'background-position': '-1232px 0px'
+        })
+    }
+})
+let zhMove = setInterval(function () {
+    $('.zh-right').click()
+}, 4000)
+$('.wanfa-daohang li').eq(0).hover(function () {
+    $(this).css({
+        'background-position': '-368px 0px'
+    })
+}, function () {
+    $(this).css({
+        'background-position': '0px 0px'
+    })
+})
+$('.wanfa-daohang li').eq(1).hover(function () {
+    $(this).css({
+        'background-position': '-426px 0px'
+    })
+}, function () {
+    $(this).css({
+        'background-position': '-58px 0px'
+    })
+})
+$('.wanfa-daohang li').eq(2).hover(function () {
+    $(this).css({
+        'background-position': '-481px 0px'
+    })
+}, function () {
+    $(this).css({
+        'background-position': '-114px 0px'
+    })
+})
+$('.wanfa-daohang li').eq(3).hover(function () {
+    $(this).css({
+        'background-position': '-538px 0px'
+    })
+}, function () {
+    $(this).css({
+        'background-position': '-169px 0px'
+    })
+})
+$('.wanfa-daohang li').eq(4).hover(function () {
+    $(this).css({
+        'background-position': '-593px 0px'
+    })
+}, function () {
+    $(this).css({
+        'background-position': '-226px 0px'
+    })
+})
+$('.wanfa-daohang li').click(function () {
+    let index = $(this).index()
+    $('.wanfa-hua img').eq(index).show().siblings().hide()
+})
+
+let wanfaIndex = 0
+$('.wanfa-right').on('click', function () {
+    wanfaIndex++
+    if (wanfaIndex > $('.wanfa-hua img').length - 1) {
+        wanfaIndex = 0
+    }
+    if (wanfaIndex == 0) {
+        $('.wanfa-daohang li').eq(wanfaIndex).css({
+            'background-position': '-368px 0px'
+        })
+    } else {
+        $('.wanfa-daohang li').eq(0).css({
+            'background-position': '0px 0px'
+        })
+    }
+    if (wanfaIndex == 1) {
+        $('.wanfa-daohang li').eq(wanfaIndex).css({
+            'background-position': '-426px 0px'
+        })
+    } else {
+        $('.wanfa-daohang li').eq(1).css({
+            'background-position': '-58px 0px'
+        })
+    }
+    if (wanfaIndex == 2) {
+        $('.wanfa-daohang li').eq(wanfaIndex).css({
+            'background-position': '-481px 0px'
+        })
+    } else {
+        $('.wanfa-daohang li').eq(2).css({
+            'background-position': '-114px 0px'
+        })
+    }
+    if (wanfaIndex == 3) {
+        $('.wanfa-daohang li').eq(wanfaIndex).css({
+            'background-position': '-538px 0px'
+        })
+    } else {
+        $('.wanfa-daohang li').eq(3).css({
+            'background-position': '-169px 0px'
+        })
+    }
+    if (wanfaIndex == 4) {
+        $('.wanfa-daohang li').eq(wanfaIndex).css({
+            'background-position': '-593px 0px'
+        })
+    } else {
+        $('.wanfa-daohang li').eq(4).css({
+            'background-position': '-226px 0px'
+        })
+    }
+    $('.wanfa-hua img').eq(wanfaIndex).fadeIn(500).siblings().fadeOut()
+    $('.wanfa-daohang li').eq(wanfaIndex).animate({
+        marginTop: 20
+    }, 500, function () {
+        $('.wanfa-daohang li').eq(wanfaIndex).siblings().animate({
+            marginTop: 0
+        }, 20)
+    })
+})
+$('.wanfa-daohang li').mouseover(function () {
+    $(this).stop().animate({
+        marginTop: 20
+    }, 500)
+})
+$('.wanfa-daohang li').mouseout(function () {
+    $(this).stop().animate({
+        marginTop: 0
+    }, 500)
+})
+let wanMove = setInterval(function () {
+    $('.wanfa-right').click()
+}, 3000)
+$('.qg-open').click(function () {
+    $('.qg-video').show()
+    $('.qg-video video').trigger('play')
+    $('body').css({
+        "overflow-y": "hidden"
+    })
+})
+$('.qg-close').click(function () {
+    $('.qg-video').hide()
+    $('.qg-video video').trigger('pause')
+    $('body').css({
+        "overflow-y": "scroll"
+    })
+})
